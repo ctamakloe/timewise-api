7b984e077438dfebb543022cf680cce70202c6a5
Christian Tamakloe <ctamakloe@gmail.com>
2020-06-08 01:08:22 +0100

    Fix secrets

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,11 +2,15 @@ class JsonWebToken
  class << self
    def encode(payload, exp = 24.hours.from_now)
      payload[:exp] = exp.to_i
-     JWT.encode(payload, Rails.application.secrets.secret_key_base)
+     secret = Rails.application.secrets.secret_key_base || ENV['SECRET_KEY_BASE']
+     JWT.encode(payload, secret)
+     # JWT.encode(payload, Rails.application.secrets.secret_key_base)
    end
 
    def decode(token)
-     body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+     secret = Rails.application.secrets.secret_key_base || ENV['SECRET_KEY_BASE']
+     body = JWT.decode(token, secret)[0]
+     # body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
      HashWithIndifferentAccess.new body
    rescue
      nil
